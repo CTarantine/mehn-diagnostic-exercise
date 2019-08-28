1498c4ab56456451fb2b7fc35c426a059ea49200
Chelsea Tarantine <asiajonesCB5@MacBook-Air-29.local>
2019-08-28 09:54:42 -0400

    gitignore added

---
+++ client/build/precache-manifest.dabd5e82f230d6b9759adcfab67b8efc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "418973043bca11d3cce3ca289b9fb27e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "61130e044f5ede8f2cdd",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "f8d2d9344fe2689b427f",
+    "url": "/static/js/2.2f9dfdf9.chunk.js"
+  },
+  {
+    "revision": "61130e044f5ede8f2cdd",
+    "url": "/static/js/main.9f808bc3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
